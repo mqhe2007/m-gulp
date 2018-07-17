95381a87ccd89e2a810ba0f731b1d1112a962d1d
青衿 <122274389@qq.com>
2018-07-17 10:15:49 +0800

    单独hash任务

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,11 @@ let gulp = require('gulp'), // gulp前端自动化工作流
   sourcemaps = require('gulp-sourcemaps'), // 生成源文件映射，便于调试
   browserSync = require('browser-sync').create(), // 浏览器同步测试工具
   reload = browserSync.reload,
-  gulpSequence = require('gulp-sequence') // 按顺序执行任务
+  gulpSequence = require('gulp-sequence'), // 按顺序执行任务
+  rev = require('gulp-rev'), // 为资源加上版本号
+  revRewrite = require('gulp-rev-rewrite'), // 把html中的资源替换为加上版本号的资源
+  babel = require('gulp-babel'),
+  revDelete = require('gulp-rev-delete-original')
 
 //设置各种输入输出文件夹的路径;
 let paths = {
@@ -52,7 +56,7 @@ gulp.task('default', function() {
 // 开启glup工作流
 gulp.task(
   'start',
-  gulpSequence('clean', ['js', 'style', 'lib', 'image'], 'html', 'server')
+  gulpSequence('clean', ['js', 'style', 'lib', 'image'], 'hash', 'html', 'server')
 )
 
 // 清空build目录
@@ -62,9 +66,16 @@ gulp.task('clean', function() {
 
 // 处理js
 gulp.task('js', function() {
+  del.sync(paths.build.scripts + '*')
+  console.log('删除历史脚本')
   return gulp
     .src(paths.src.scripts + '**/*.js')
     .pipe(gulpif(process.env.NODE_ENV === 'production', sourcemaps.init()))
+    .pipe(
+      babel({
+        presets: ['@babel/env']
+      })
+    )
     .pipe(gulpif(process.env.NODE_ENV === 'production', uglify()))
     .pipe(gulpif(process.env.NODE_ENV === 'production', sourcemaps.write()))
     .pipe(gulp.dest(paths.build.scripts))
@@ -80,18 +91,30 @@ gulp.task('lib', function() {
 
 //处理样式表
 gulp.task('style', function() {
+  del.sync(paths.build.styles + '*')
+  console.log('删除历史样式表')
   return gulp
     .src(paths.src.styles + '*.styl')
     .pipe(sourcemaps.init())
     .pipe(stylus())
-    .pipe(
-      autoprefixer({
-        browsers: ['last 2 versions', 'iOS', 'Android']
-      })
-    )
-    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(paths.build.styles))
     .pipe(reload({ stream: true }))
+})
+
+// 为css,js添加版本号
+
+gulp.task('hash', () => {
+  del.sync(paths.build.html + 'public/rev-manifest.json')
+  console.log('删除历史替换清单')
+  return gulp
+    .src([paths.build.scripts + '**/*', paths.build.styles + '**/*'], {base: 'build'})
+    .pipe(rev())
+    .pipe(revDelete())
+    .pipe(gulp.dest('build'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('build/public'))
 })
 
 //压缩图片
@@ -117,9 +140,11 @@ gulp.task('image', function() {
 
 //编译html
 gulp.task('html', function() {
+  const manifest = gulp.src('build/public/rev-manifest.json')
   return gulp
     .src(paths.src.html + '**/*.html')
     .pipe(changedInPlace({ firstPass: true }))
+    .pipe(revRewrite({ manifest }))
     .pipe(gulp.dest(paths.build.html))
     .pipe(reload({ stream: true }))
 })
@@ -131,9 +156,12 @@ gulp.task('server', function() {
       baseDir: 'build'
     }
   })
-  gulp.watch(paths.src.scripts + '*.js', ['js'])
-  gulp.watch(paths.src.libs + '**/*.min.js', ['lib'])
-  gulp.watch(paths.src.styles + '*.styl', ['style'])
-  gulp.watch(paths.src.images + '**/*.{png,jpg,gif}', ['image'])
-  gulp.watch(paths.src.html + '**/*.html', ['html'])
+  gulp.watch('*.js', { cwd: paths.src.scripts }, ['js'])
+  gulp.watch('**/*.min.js', { cwd: paths.src.libs }, ['lib'])
+  gulp.watch('*.styl', { cwd: paths.src.styles }, function(event) {
+    console.log(event.type)
+    gulpSequence('style', 'hash', 'html')()
+  })
+  gulp.watch('**/*.{png,jpg,gif}', { cwd: paths.src.images }, ['image'])
+  gulp.watch('**/*.html', { cwd: paths.src.html }, ['html'])
 })
